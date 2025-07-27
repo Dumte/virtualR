89910926afe0dd110e7771d7483d5f5df4d75e18
dumte <dummy4real@gmail.com>
2025-07-27 23:15:18 +0100

    feat: add Pricing component and integrate it into App, removing redundant pricing options from constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import Workflow from "./components/Workflow";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <HeroSection />
         <Features />
         <Workflow />
+        <Pricing />
       </div>
     </>
   );
